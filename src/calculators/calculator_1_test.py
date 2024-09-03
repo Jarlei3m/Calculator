770c4c176f275db7b199bf51d6b32d9f0c19f8cb
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2024-09-03 11:50:37 -0300

    #test: first calculator unit test for body error exceptions

--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from pytest import raises
 from .calculator_1 import Calculator1
 
 class MockRequest:
@@ -23,5 +24,14 @@ def test_calculate():
   assert response["data"]["Calculator"] == 1
   assert response["data"]["result"] == 14.25
 
-  def test_calculate_with_body_error():
-    pass
+def test_calculate_with_body_error():
+  mock_body = {
+    "something": 1
+  }
+  mock_request = MockRequest(body=mock_body)
+  calculator_1 = Calculator1()
+
+  with raises(Exception) as excinfo:
+    calculator_1.calculate(mock_request)
+
+  assert str(excinfo.value) == "Invalid body format"
