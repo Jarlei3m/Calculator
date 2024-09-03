d73fb057d531c2c9dedf69190116bf88a2843abf
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2024-09-03 11:39:58 -0300

    test: first calculator unit test

---
+++ src/calculators/calculator_1_test.py
@@ -0,0 +1,27 @@
+from typing import Dict
+from .calculator_1 import Calculator1
+
+class MockRequest:
+  def __init__(self, body: Dict) -> None:
+    self.json = body
+
+def test_calculate():
+  mock_body = {
+    "number": 1
+  }
+  mock_request = MockRequest(body=mock_body)
+  
+  calculator_1 = Calculator1()
+  response = calculator_1.calculate(mock_request)
+
+  # Response format
+  assert "data" in response
+  assert "Calculator" in response["data"]
+  assert "result" in response["data"]
+
+  # Response assert
+  assert response["data"]["Calculator"] == 1
+  assert response["data"]["result"] == 14.25
+
+  def test_calculate_with_body_error():
+    pass
